c3c046da4f8a0e607c3a600f42d2f4ee5d85fb34
TE.undeaD_D <undeaD_D@live.de>
2019-07-05 14:29:44 +0200

    added cms contact and login functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,3 +13,29 @@ firebase.initializeApp(firebaseConfig);
 document.oncontextmenu = function () {
 	return window.location.href.indexOf('http://127.0.0.1') == 0;
 };
+
+try {
+	firebase.auth().onAuthStateChanged(function (user) {
+		if (user) {
+			if (window.location.href.includes("/cms/admin.html")) {
+				// if logged in but on admin page -> redirect to cms page
+				window.location.href = "/cms/contact.html";
+			}
+		} else {
+			if (!window.location.href.includes("/cms/admin.html")) {
+				// if on any other cms page but not logged in -> redirect to admin page
+				window.location.href = "/cms/admin.html";
+			}
+		}
+	});
+} catch (e) { }
+
+function login() {
+	var email = document.getElementById("inputEmail").value;
+	var password = document.getElementById("inputPassword").value;
+	firebase.auth().signInWithEmailAndPassword(email, password);
+}
+
+function logout() {
+	firebase.auth().signOut();
+}
