64209c3aab05df8ca3212bb46620a63f22bcb1d5
TE.undeaD_D <undeaD_D@live.de>
2019-07-07 22:27:42 +0200

    break all the points for responsiveness :3

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,12 +16,13 @@ document.oncontextmenu = function () {
 	return window.location.href.indexOf('http://127.0.0.1') == 0;
 };
 
-function login() {
+function login(form) {
 	try {
 		var email = document.getElementById("inputEmail").value;
 		var password = document.getElementById("inputPassword").value;
 		firebase.auth().signInWithEmailAndPassword(email, password);
 	} catch (e) { }
+	return false;
 }
 
 function logout(site) {
@@ -30,11 +31,20 @@ function logout(site) {
 	} catch (e) { }
 }
 
+function checkEnter(event) {
+	try {
+		if (event.keyCode === 13) {
+			event.preventDefault();
+			document.getElementById("loginBtn").click();
+		}
+	} catch (e) { }
+}
+
 try {
 	firebase.auth().onAuthStateChanged(function (user) {
 		if (user) {
 			if (window.location.href.includes("/cms_admin.html")) {
-				window.location.href = window.location.href.replace("cms_admin.html", "cms_blog.html");
+				window.location.href = window.location.href.replace("cms_admin.html", "cms_index.html");
 			}
 			try {
 				document.getElementById("userAvatar").src = "assets/img/" + user.uid + ".png"
@@ -54,7 +64,7 @@ try {
 		event.preventDefault();
 		var base = "aHR0cHM6Ly9hcGkudGVsZWdyYW0ub3JnL2JvdDg3MDcyNDQ1NDpBQUc4Mkg0ZlpXa1pycVlnUlMzNmcwLUpUbHpNS3dFbk96WS9zZW5kTWVzc2FnZQ==";
 		var message = "<b>[Valo's Contact Request]</b>\n\n<b>Name:</b> " + document.getElementById("name").value + "\n<b>Reply via:</b> " + document.getElementById("email").value + "\n\n<b>Message:</b>\n" + document.getElementById("message").value;
-		$.each(["20932747"/*, "16489403"*/], function (index, id) {
+		$.each(["20932747", "16489403"], function (index, id) {
 			$.ajax({
 				url: encodeURI(atob(base) + "?chat_id=" + id + "&parse_mode=HTML&disable_web_page_preview=true&text=" + message),
 				async: false
@@ -66,6 +76,22 @@ try {
 
 try {
 	if (window.location.href.includes("/index.html")) {
+		firebase.database().ref('index').on('value', function (snapshot) {
+			try {
+				document.getElementById("IndexID").innerHTML = "";
+			} catch (e) { }
+
+			for (const [index, item] of JSON.parse(snapshot.val()).entries()) {
+				document.getElementById('IndexID').innerHTML += `
+				<div class="` + item["style"] + `" style="height: 50vh;">
+					<a href="project.html?id=` + index + `">
+						<div class="project shadow-lg" style="background-image: url('assets/img/projects/` + item["image"] + `');"></div>
+					</a>
+				</div>
+				`;
+			}
+		});
+
 		if (new URL(window.location.href).searchParams.get("sendMessage") !== null) {
 			$("#sendMessage").hide().delay(1000).fadeIn(200).delay(3500).fadeOut(1000, function () {
 				window.history.replaceState(null, null, window.location.pathname);
@@ -76,13 +102,11 @@ try {
 
 try {
 	if (window.location.href.includes("/contact.html")) {
-		var ref = firebase.database().ref('info');
-		ref.on('value', function (snapshot) {
+		firebase.database().ref('info').on('value', function (snapshot) {
 			document.getElementById("FB-Info").innerHTML = "<h2 class=\"card-title mb-4\">Informations</h2>" + snapshot.val();
 		});
 
-		var ref = firebase.database().ref('contactEnabled');
-		ref.on('value', function (snapshot) {
+		firebase.database().ref('contactEnabled').on('value', function (snapshot) {
 			if (snapshot.val()) {
 				document.getElementById("contactEnabled").innerHTML = "<i class=\"fas fa-lg fa-share\"></i> Submit";
 				document.getElementById("contactEnabled").classList.remove("btn-danger");
@@ -98,12 +122,32 @@ try {
 	}
 } catch (e) { }
 
+function saveHome() {
+	try {
+		if (window.location.href.includes("/cms_index.html")) {
+			firebase.database().ref('index').set(document.getElementById('HomeID').value);
+		}
+	} catch (e) { }
+}
+
 try {
-	if (window.location.href.includes("/cms_faq.html")) {
-		function saveInfo() {
+	if (window.location.href.includes("/cms_index.html")) {
+		firebase.database().ref('index').on('value', function (snapshot) {
+			document.getElementById('HomeID').value = snapshot.val();
+		});
+	}
+} catch (e) { }
+
+function saveFAQ() {
+	try {
+		if (window.location.href.includes("/cms_faq.html")) {
 			firebase.database().ref('faq').set(document.getElementById('FAQID').value);
 		}
+	} catch (e) { }
+}
 
+try {
+	if (window.location.href.includes("/cms_faq.html")) {
 		firebase.database().ref('faq').on('value', function (snapshot) {
 			document.getElementById('FAQID').value = snapshot.val();
 		});
@@ -156,13 +200,11 @@ function toggle() {
 
 try {
 	if (window.location.href.includes("/cms_contact.html")) {
-		var ref = firebase.database().ref('info');
-		ref.on('value', function (snapshot) {
+		firebase.database().ref('info').on('value', function (snapshot) {
 			document.getElementById("FB-Info").value = snapshot.val();
 		});
 
-		var ref = firebase.database().ref('contactEnabled');
-		ref.on('value', function (snapshot) {
+		firebase.database().ref('contactEnabled').on('value', function (snapshot) {
 			if (snapshot.val()) {
 				document.getElementById("contactEnabled").innerText = "Status: enabled"
 				document.getElementById("contactEnabled").classList.remove("btn-danger");
@@ -183,7 +225,7 @@ try {
 				document.getElementById("twitterScriptID").remove();
 			} catch (e) { }
 
-			document.getElementById("TwitterBlogURL").innerHTML = "<div class=\"card shadow\" style=\"background-color: #292F33;\"><div class=\"card-body\" ><a class=\"twitter-timeline\" href=\"" + snapshot.val() + "\" data-theme=\"dark\" data-border-color=\"#292F33\" data-link-color=\"#ffffff\" data-chrome=\"noborders transparent nofooter noscrollbar\" data-dnt=\"true\" data-tweet-limit=\"2\"><\/a></div></div>";
+			document.getElementById("TwitterBlogURL").innerHTML = "<div class=\"card shadow mb-4\" style=\"background-color: #292F33;\"><div class=\"card-body\" ><h2 class=\"card-title mb-4\">Twitter Posts</h2><a class=\"twitter-timeline\" href=\"" + snapshot.val() + "\" data-theme=\"dark\" data-border-color=\"#292F33\" data-link-color=\"#ffffff\" data-chrome=\"noborders transparent nofooter noheader noscrollbar\" data-dnt=\"true\" data-tweet-limit=\"2\"><\/a></div></div>";
 			var script = document.createElement('script');
 			script.id = "twitterScriptID"
 			script.src = "https://platform.twitter.com/widgets.js";
@@ -198,7 +240,7 @@ try {
 			for (const post of JSON.parse(snapshot.val())) {
 				var data = `<div class="card mb-4 bg-dark shadow">
 					<div class="card-body">
-						<h2 class="card-title">` + post["title"] + `</h2>
+						<h2 class="card-title mb-4">` + post["title"] + `</h2>
 						<p class="card-text">` + post["body"] + `</p>
 					</div>
 					<div class="card-footer bg-black-transparent text-muted">
@@ -213,12 +255,3 @@ try {
 		});
 	}
 } catch (e) { }
-
-function checkEnter(event) {
-	try {
-		if (event.keyCode === 13) {
-			event.preventDefault();
-			document.getElementById("loginBtn").click();
-		}
-	} catch (e) { }
-}
